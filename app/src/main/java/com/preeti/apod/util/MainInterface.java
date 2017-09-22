61476aed289bcf9fab4a2af6bc86f861c39d32c8
Preeti <pwadhwani2592@gmail.com>
2017-09-22 14:47:43 +0530

    Initial Commit

---
+++ app/src/main/java/com/preeti/apod/util/MainInterface.java
@@ -0,0 +1,21 @@
+package com.preeti.apod.util;
+
+/**
+ * Created by Game on 22-09-17.
+ */
+
+public interface MainInterface {
+
+    public interface MainView {
+
+    }
+
+    public interface MainPresenter {
+
+    }
+
+    public interface mainModel {
+
+    }
+
+}
